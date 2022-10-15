74f3b467876d823f08e5950185ce222551a8ec3e
claudia pimentel <claudia.pimentel@pagar.me>
2022-10-14 21:30:49 -0300

    add a exemple view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
-import { useState } from 'react'
+import { Logo } from './Logo';
+import { Text } from './components/Text';
+import { Heading } from './components/Heading';
+import { Envelope, Lock } from 'phosphor-react';
 import './styles/global.css';
+import { TextInput } from './components/TextInput';
+import { Checkbox } from './components/Checkbox';
+import { Button } from './components/Button';
 
 export function App() {
   return (
-    <h1 className="font-bold text-5xl text-violet-500" >Oie</h1>
+    <div className='w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100'>
+      <header className='flex flex-col items-center'>
+        <Logo />
+
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>  
+
+        <Text size="lg" className='text-gray-400 mt-1'>Faça seu login</Text>
+      </header>
+      <form className='flex flex-col items-stretch w-full max-w-[400px] mt-10 gap-4'>
+        <label htmlFor='email' className='flex flex-col gap-3'>
+          <Text className='font-semibold'>Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input  placeholder='E-mail' id="e-mal"/>
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor='password' className='flex flex-col gap-3'>
+          <Text className='font-semibold'>Senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input  placeholder='****' type="password" id="password" />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor='remember' className='flex items-center gap-2'>
+          <Checkbox id="remember"/>
+          <Text size="sm" className='text-gray-200'>Lembrar de mim por 30 dias</Text>
+        </label>
+
+        <Button type='submit' className='mt-4'>Entrar na plataforma</Button>
+      </form>
+    </div>
   )
 }
 
